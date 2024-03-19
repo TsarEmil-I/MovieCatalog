d7420eb9556a56c4b3bcf28d00ff1b186059d4c9
krisito2000 <krisi.sirakov@abv.bg>
2024-03-19 11:27:53 +0200

    Add project files.

---
+++ Data/Catalog.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalog.Data
+{
+    public class Catalog : BaseEntity
+    {
+        [MinLength(2)]
+        [MaxLength(64)]
+        public string Name { get; set; }
+
+        [MinLength(0)]
+        [MaxLength(255)]
+        public string Description { get; set; }
+        public List<Movie> Movies { get; set; }
+    }
+}
---
+++ Data/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace MovieCatalog.Data
+{
+    public class Movie : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+        public string Description { get; set; }
+        public List<int> Ratings { get; set; }
+    }
+}
